8e2793dd295ff98bdbcfcbf530f759e2c0aaf2c7
Priyadharshini23-hub <priyadarshiniselvanathan@gmail.com>
2025-05-20 13:45:55 +0530

    Add files via upload

---
+++ PageNotFound.jsx
@@ -0,0 +1,13 @@
+import image from "../assets/image.png"
+import { Link } from "react-router-dom"
+import Login from "./Login"
+ const PageNotFound = () => {
+  return (
+    <div>
+        <h1>This page is not found</h1>
+        <img className="w-100" src={image} alt="404"/>
+        <Link to="/Login">Login</Link>
+    </div>
+  )
+}
+export default PageNotFound
